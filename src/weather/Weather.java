af0fa9db5e6137a2563c32ab74972f65631d1b4b
Andrés <insane-stdios@hotmail.com>
2015-03-05 22:13:54 -0600

    Weather of Mexico

---
+++ src/weather/Weather.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package weather;
+
+/**
+ *
+ * @author 7h3n3w3r4cr4ck3r
+ */
+public class Weather {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Mexico weather = new Mexico();
+        weather.setVisible(true);
+    }
+    
+}
